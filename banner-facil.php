36844649544506b5ce282afe71ca4c21aae383a4
jeancarlosln <jean.carlos.cmo@gmail.com>
2023-04-30 21:45:38 -0300

    Atualizações de cabeçalho e licença

--- banner-facil.php
+++ banner-facil.php
@@ -1,10 +1,29 @@
 <?php
 /*
-Plugin Name: Banner Fácil
-Description: Simplifique a inserção de banners no seu site. Não dependa mais de profissionais, você mesmo pode inserir, gerenciar e remove quando quiser ou programe de forma automática. Insira link, imagem e scripts de implementação. Gerencie o posicionamento e a área de exibição de forma simples e rápida.
-Version: 1.0
-Author: Uios
-*/
-
+ * Plugin Name:       Banner Fácil
+ * Plugin URI:        https://uios.com.br/plugins/banner-facil/
+ * Description:       Banner para gerenciar banners de campanhas publicitárias.
+ * Version:           0.1.1
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Jean C. Lima
+ * Author URI:        https://jeanclima.com.br/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       banner-facil
+ * O Banner Fácil é um software gratuito: você pode redistribuí-lo e/ou modificá-lo
+ * sob os termos da GNU General Public License publicada por
+ * a Free Software Foundation, versão 3 da Licença, ou
+ * (à sua escolha) qualquer versão posterior.
+ *
+ * O Banner Fácil é distribuído na esperança de que seja útil,
+ * mas SEM QUALQUER GARANTIA; mesmo sem a garantia implícita de
+ * COMERCIABILIDADE ou ADEQUAÇÃO PARA UM FIM ESPECÍFICO. Veja o
+ * GNU General Public License para mais detalhes.
+ *
+ * Você deve ter recebido uma cópia da GNU General Public License
+ * junto com o Banner Fácil. Caso contrário, consulte <https://www.gnu.org/licenses/>.
+ *
+ */
 
 ?>
